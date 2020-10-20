6fa138faf02c3d61f2b8facadb1c1f8eff752823
sghnsk <sgh.nsk@gmail.com>
2020-10-21 02:16:24 +0700

    Added sghnsk keymaps with default keyamps settings

---
+++ keyboards/converter/usb_usb/keymaps/sghnsk/rules.mk
@@ -0,0 +1,4 @@
+BOOTMAGIC_ENABLE = full  # Virtual DIP switch configuration
+CONSOLE_ENABLE   = yes  # Console for debug
+COMMAND_ENABLE   = yes  # Commands for debug and configuration
+NKRO_ENABLE      = yes  # USB Nkey Rollover - not yet supported in LUFA
